e0143c13676875455ee942972ee255d50e04f3d2
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2022-11-23 10:09:11 +0200

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            
+        </div>
+    );
+};
+
+export default Header;
